f8a5adacb1592b8db40209f2390393e18f052844
sanaulla0 <vadapallisanaulla@gmail.com>
2023-06-07 22:50:22 +0530

    submitting code-Wed Jun 07 2023 22:50:21 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[flag,setFlag] = useState(false);
+  const handleShow = ()=>{
+           setFlag(true);
+  }
+  const handleClose = () =>{
+       setFlag(false)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        
+        <button className="btn" onClick={handleShow} >Show Model</button> <br/> &nbsp;
+        {flag ? ( <div className="model-overlay">
+           <button className="model-close" onClick={handleClose}>Close</button>
+           <p>This is the content of the model</p>
+       </div>) : ("")}
+      
+
     </div>
   )
 }
